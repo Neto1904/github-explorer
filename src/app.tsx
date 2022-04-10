c43f983a82d961b5474e1300175d0b732f313794
Neto1904 <netohsa66@gmail.com>
2022-04-10 20:41:04 -0300

    Reafctoring code to use typescript

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from './components/RespositoryList'
+import './styles/globals.scss'
+
+
+export function App() {
+    return (
+        <>
+            <RepositoryList />
+        </>
+    )
+}
